922d42eb07ebdecb074b8167a7f11eafc2a21c96
Bruno P.S <bruno.ps@ufba.br>
2024-06-25 17:08:10 -0300

    Add mobnet app

---
+++ apps/mobnet/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MobnetConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.mobnet'
--- mobcore/settings.py
+++ mobcore/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "apps.frontend",
+    "apps.mobnet",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'mobcore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
