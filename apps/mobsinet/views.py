cb0006916a12b60f3fefd51ab0e4858fc9089e7f
Bruno P.S <bruno.ps@ufba.br>
2024-07-05 14:29:47 -0300

    mobsinet: reset views

--- apps/mobsinet/views.py
+++ apps/mobsinet/views.py
@@ -1,25 +1,22 @@
 from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
-from .sim.net import graph
-from .sim.runsim import runsim
+#from .sim.net import graph
 
 # Create your views here.
 def index(request):
-
-    runsim()
 
     return render(request, "mobsinet_index.html")
 
 
 def graph_view(request):
+    pass
+    #graph_div = graph.graph_view(request)
 
-    graph_div = graph.graph_view(request)
-
-    return render(request, 'graph.html', {'graph_div': graph_div})
+    #return render(request, 'graph.html', {'graph_div': graph_div})
 
 
 def update_graph(request):
+    pass
+    #graph_div = graph.update_graph()
 
-    graph_div = graph.update_graph()
-
-    return JsonResponse({'graph_div': graph_div})
+    #return JsonResponse({'graph_div': graph_div})
