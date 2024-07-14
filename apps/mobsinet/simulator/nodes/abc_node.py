649ef09a3c216755ac2d444d24051034a15d79f0
João Victor <jv.joaocoelho@gmail.com>
2024-07-14 11:56:55 -0300

    update abstract node

--- apps/mobsinet/simulator/nodes/abc_node.py
+++ apps/mobsinet/simulator/nodes/abc_node.py
@@ -1,20 +1,58 @@
 from ..tools.position import Position
 from ..models.abc_mobility_model import AbcMobilityModel
+from ..models.abc_connectivity_model import AbcConnectivityModel
+from ..models.abc_interference_model import AbcInterferenceModel
+from ..models.abc_reliability_model import AbcReliabilityModel
+from abc import ABC, abstractmethod
 
-class AbcNode(object):
 
-    def __init__(self, id):
+class AbcNode(ABC):
+
+    def __init__(
+            self,
+            id: int,
+            position: Position = None,
+            mobility_model: AbcMobilityModel = None,
+            connectivity_model: AbcConnectivityModel = None,
+            interference_model: AbcInterferenceModel = None,
+            reliability_model: AbcReliabilityModel = None):
         self.id = id
-        self.position: Position = None
-        self.mobility_model: AbcMobilityModel = None
-        # self.connectivity_model: AbcConnectivityModel = None
-        # self.interference_model: AbcInterferenceModel = None
-        # self.reliability_model: AbcReliabilityModel = None
+        self.position: Position = position
+        self.mobility_model: AbcMobilityModel = mobility_model
+        self.connectivity_model: AbcConnectivityModel = connectivity_model
+        self.interference_model: AbcInterferenceModel = interference_model
+        self.reliability_model: AbcReliabilityModel = reliability_model
 
     def __str__(self):
         return f"""
-        ID: {self.id}
-        Position: {self.position}
-        Mobility Model: {self.mobility_model.name}
-        """
-        
+ID: {self.id}
+Position: {self.position}
+Mobility Model: {self.mobility_model.name}
+Connectivity Model: {self.connectivity_model.name}
+Interference Model: {self.interference_model.name}
+Reliability Model: {self.reliability_model.name}
+"""
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def set_position(self, position: Position):
+        self.position = position
+
+    def set_mobility_model(self, mobility_model: AbcMobilityModel):
+        self.mobility_model = mobility_model
+
+    def set_connectivity_model(self, connectivity_model: AbcConnectivityModel):
+        self.connectivity_model = connectivity_model
+
+    def set_interference_model(self, interference_model: AbcInterferenceModel):
+        self.interference_model = interference_model
+
+    def set_reliability_model(self, reliability_model: AbcReliabilityModel):
+        self.reliability_model = reliability_model
+
+    def get_coordinates(self):
+        return self.position.get_coordinates()
+
+    def set_coordinates(self, x: int, y: int, z: int):
+        return self.position.set_coordinates(x, y, z)
