49472f7c018ab7daad07be060606f50e1fc61b66
João Victor <jv.joaocoelho@gmail.com>
2024-07-18 10:47:41 -0300

    document default models

--- apps/mobsinet/simulator/defaults/distribution_models/random_dist.py
+++ apps/mobsinet/simulator/defaults/distribution_models/random_dist.py
@@ -3,23 +3,33 @@ from ...tools.position import Position
 from ...models.abc_distribution_model import AbcDistributionModel
 from ...configuration.sim_config import sim_config_env
 
-#SEED such seed could be configured from config file to reproduce simulation.
-#seed_value = 10
-#random.seed(seed_value)
+# SEED such seed could be configured from config file to reproduce simulation.
+# seed_value = 10
+# random.seed(seed_value)
 
 
 class RandomDist(AbcDistributionModel):
+    """A random Distribution Model."""
 
     def __init__(self):
         super().__init__("RandomDist")
 
     def get_position(self) -> Position:
-        p = Position(x= random.randrange(0, sim_config_env.dimX),
-                     y= random.randrange(0, sim_config_env.dimZ),
-                     z= random.randrange(0, sim_config_env.dimZ))
+        """
+        Generate a random position within the given dimensions.
+
+        Returns
+        -------
+        Position: The randomly generated position.
+        """
+
+        p = Position(x=random.randrange(0, sim_config_env.dimX),
+                     y=random.randrange(0, sim_config_env.dimZ),
+                     z=random.randrange(0, sim_config_env.dimZ))
 
         return p
-    
+
+
 model = RandomDist
 
 if __name__ == "__main__":
--- apps/mobsinet/simulator/defaults/mobility_models/random_mob.py
+++ apps/mobsinet/simulator/defaults/mobility_models/random_mob.py
@@ -3,16 +3,27 @@ from ...models.abc_mobility_model import AbcMobilityModel
 from random import randint
 from ...configuration.sim_config import sim_config_env
 
+
 class RandomMob(AbcMobilityModel):
+    """A random mobility model."""
 
     def __init__(self):
         super().__init__('RandomMob')
 
     def get_next_position(self) -> Position:
+        """
+        Generate a random position within the given dimensions.
+
+        Returns
+        -------
+        Position: The randomly generated position.
+        """
+
         xcord = randint(0, sim_config_env.dimX)
         ycord = randint(0, sim_config_env.dimY)
         zcord = randint(0, sim_config_env.dimZ)
 
         return Position(xcord, ycord, zcord)
-    
+
+
 model = RandomMob
