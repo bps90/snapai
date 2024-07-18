5b034f9c9edf96b5e880f35b73d8893a4136b1bc
João Victor <jv.joaocoelho@gmail.com>
2024-07-18 13:31:59 -0300

    document abstract models

--- apps/mobsinet/simulator/models/abc_distribution_model.py
+++ apps/mobsinet/simulator/models/abc_distribution_model.py
@@ -1,13 +1,18 @@
 from abc import abstractmethod
+
+from ..models.nodes.abc_node_behavior import AbcNodeBehavior
 from ..tools.position import Position
 from .abc_model import AbcModel
+
 
 class AbcDistributionModel(AbcModel):
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
-    
-    
+
     @abstractmethod
-    def get_position(self) -> Position:
+    def get_position(self, node_behavior: AbcNodeBehavior) -> Position:
+        """(abstract) This method should return a Position object that 
+        represents the first position of the node."""
+
         pass
--- apps/mobsinet/simulator/models/abc_mobility_model.py
+++ apps/mobsinet/simulator/models/abc_mobility_model.py
@@ -1,12 +1,23 @@
 from abc import abstractmethod
+
+
 from ..tools.position import Position
 from .abc_model import AbcModel
 
+
 class AbcMobilityModel(AbcModel):
 
-    def __init__(self, name: str): 
+    def __init__(self, name: str):
         super().__init__(name)
 
     @abstractmethod
-    def get_next_position(self) -> Position:
+    def get_next_position(self, node_behavior) -> Position:
+        """(abstract) This method should return the next position for the node.
+
+        Parameters
+        ----------
+        node_behavior : AbcNodeBehavior
+            The node behavior object.
+        """
+
         pass
