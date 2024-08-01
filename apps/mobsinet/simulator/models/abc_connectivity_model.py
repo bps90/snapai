1e5e7b4d1fa2e57db46cab6d50d06a706616cb32
JoaoSCoelho <jv.joaocoelho@gmail.com>
2024-08-01 08:10:34 -0300

    starts connectivity model implementation

--- apps/mobsinet/simulator/models/abc_connectivity_model.py
+++ apps/mobsinet/simulator/models/abc_connectivity_model.py
@@ -1,4 +1,15 @@
+from abc import abstractmethod
+
+from apps.mobsinet.simulator.models.nodes.abc_node_behavior import AbcNodeBehavior
 from .abc_model import AbcModel
 
+
 class AbcConnectivityModel(AbcModel):
-    pass
+
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    @abstractmethod
+    def is_connected(self, node_a: AbcNodeBehavior, node_b: AbcNodeBehavior) -> bool:
+        """Check if the nodes are connected."""
+        pass
