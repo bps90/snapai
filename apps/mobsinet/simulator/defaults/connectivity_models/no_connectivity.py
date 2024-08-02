1eee42adca502a0ef455d056d4ff20c0225e0de1
João Victor <jv.joaocoelho@gmail.com>
2024-08-02 09:09:36 -0300

    implements NoConnectivity

--- apps/mobsinet/simulator/defaults/connectivity_models/no_connectivity.py
+++ apps/mobsinet/simulator/defaults/connectivity_models/no_connectivity.py
@@ -1,7 +1,14 @@
+from apps.mobsinet.simulator.models.nodes.abc_node_behavior import AbcNodeBehavior
 from ...models.abc_connectivity_model import AbcConnectivityModel
+
 
 class NoConnectivity(AbcConnectivityModel):
     def __init__(self):
         super().__init__('NoConnectivity')
+
+    def is_connected(self, node_a: AbcNodeBehavior, node_b: AbcNodeBehavior) -> bool:
+        """Check if the nodes are connected."""
+        return False
+
 
 model = NoConnectivity
