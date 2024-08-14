431acd4c65c6faab18eb100b4b2b7249bb25584e
João Victor <jv.joaocoelho@gmail.com>
2024-08-14 10:37:57 -0300

    implements reliable delivery

--- apps/mobsinet/simulator/defaults/reliability_models/reliable_delivery.py
+++ apps/mobsinet/simulator/defaults/reliability_models/reliable_delivery.py
@@ -2,13 +2,13 @@ from apps.mobsinet.simulator.models.nodes.abc_packet import AbcPacket
 from ...models.abc_reliability_model import AbcReliabilityModel
 
 
-class NoReliability(AbcReliabilityModel):
+class ReliableDelivery(AbcReliabilityModel):
 
     def __init__(self):
-        super().__init__('NoReliability')
+        super().__init__('ReliableDelivery')
 
     def reaches_destination(self, packet: AbcPacket):
-        return False
+        return True
 
 
-model = NoReliability
+model = ReliableDelivery
--- apps/mobsinet/simulator/models/abc_reliability_model.py
+++ apps/mobsinet/simulator/models/abc_reliability_model.py
@@ -1,4 +1,11 @@
+from abc import abstractmethod
+
+from apps.mobsinet.simulator.models.nodes.abc_packet import AbcPacket
 from .abc_model import AbcModel
 
+
 class AbcReliabilityModel(AbcModel):
-    pass
+
+    @abstractmethod
+    def reaches_destination(self, packet: AbcPacket) -> bool:
+        pass
